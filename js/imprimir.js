5d7f57109fc84e5c3e49d3884fe2b6404b1a0def
tetraphoz <tetraphosphorus@gmail.com>
2023-02-25 10:43:56 -0600

    Algunas actualizaciones :)

--- js/imprimir.js
+++ js/imprimir.js
@@ -1,13 +1,32 @@
-// Archivo de Javascript :^)
+let usuarioTXT = document.getElementById("usuario");
+let passwordTXT = document.getElementById("campoPassword");
+let p = document.getElementById("mensaje");
 
-var campoTexto = document.getElementById("campoTexto");
 
-console.log(campoTexto);
+function togglePassword(){
+    var campo = document.getElementById("campoPassword");
+    var mensaje = document.getElementById("vistaPassword");
 
-function imprimir(){
-    console.log(campoTexto.value);
+    if(campo.type == "password"){
+        campo.type = "text";
+        mensaje.textContent = "Ocultar Password";
+    } else{
+        campo.type = "password";
+        mensaje.textContent = "Mostrar Password";
+    }
+}
+
+function login(){
+    p.innerHTML = "Login correcto, " + usuarioTXT.value;
+    limpiar();
+}
+
+function registro(){
+    p.innerHTML = "Registro correcto, " + usuarioTXT.value;
+    limpiar();
 }
 
 function limpiar(){
-    campoTexto.value = "";
+    usuarioTXT.value = "";
+    passwordTXT.value = "";
 }
