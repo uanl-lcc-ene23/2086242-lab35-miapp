ff32e4df735eb6e2eb5320cab4e4735da7a4f0ba
tetraphoz <tetraphosphorus@gmail.com>
2023-03-11 11:30:42 -0600

    Agregar el componente lista-alumnos

--- miApp/src/app/app.module.ts
+++ miApp/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { IonicModule } from '@ionic/angular';
+import { ListaAlumnosComponent } from './lista-alumnos/lista-alumnos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListaAlumnosComponent
   ],
   imports: [
     BrowserModule,
---
+++ miApp/src/app/lista-alumnos/lista-alumnos.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lista-alumnos', // <app-lista-alumnos>
+  templateUrl: './lista-alumnos.component.html',
+  styleUrls: ['./lista-alumnos.component.css']
+})
+export class ListaAlumnosComponent {
+  nombre = "Adrian";
+  alumnos = ["Carlos", "Ximena", "Alberto", "Eduardo"];
+}
